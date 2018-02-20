22e8304b4cf9c9275e57e8d4608964c49d39fe16
Sondre Haug <sondr.haug@gmail.com>
2018-02-20 12:09:24 +0800

    Restructure, add I controller and fix bugs

--- src/node_controller.cpp
+++ src/node_controller.cpp
@@ -13,189 +13,154 @@
 using namespace std_msgs;
 
 class Control{
+private:
+    ros::Publisher pub_control_speed;
+    ros::Subscriber sub_setpoint;
+    ros::Subscriber sub_position;
 
-        private:
+    double trans_x, trans_heading, I_heading, I_pos, dt;
+    double pos_x, pos_y, heading, setpoint_x, setpoint_y, setpoint_heading;
+    double error_x, error_y, error_heading, error_pos;
+    double PI, Kp_fwd, Kp_ang, Ki_ang, Ki_fwd;
+public:
+    Control(ros::NodeHandle nh);
 
-            //topics
-            ros::Publisher pub_control_speed;
-            ros::Subscriber sub_setpoint;
-            ros::Subscriber sub_position;
-
-            //variables
-            double trans_x, trans_z;
-            double pos_x, pos_y, ori_z, ang_z,set_x,set_y,set_z;
-            double error_x, error_y, error_z, error_dist;
-
-        public:
-
-            /* Constructor */
-            Control(ros::NodeHandle nh);
-
-            /* Constants */
-            float PI;
-
-            //Forward gain
-            double Kp_fwd = 10.0;
-
-
-            //Heading gain
-            double Kp_ang = 0.05;
-
-
-            //Functions
-
-            //This function will subscribe to setpoint messages from
-            //the pathplanner node.
-            void get_setpoint(const geometry_msgs::Point& setpointMsg);
-            void get_position(const geometry_msgs::Point& posMsg);
-            void calculate_speed();
+    void get_setpoint(const geometry_msgs::Point& setpointMsg);
+    void get_position(const geometry_msgs::Point& posMsg);
+    void calculate_speed();
+    void spin();
 };
 
+//Constructor
+Control::Control(ros::NodeHandle nh) {
+
+    //Initialize
+    pos_x = 0;
+    pos_y = 0;
+    heading = 0;
+    setpoint_x = 0;
+    setpoint_y = 0;
+    setpoint_heading = 0;
+    error_x = 0;
+    error_y = 0;
+    error_heading = 0;
+    error_pos = 0;
+    PI = 3.14159265359;
+    Kp_fwd = 0.2; //Forward gain
+    Kp_ang = 0.5; //Heading gain
+    I_heading = 0;
+    I_pos = 0;
+    dt = 0.1;
+    Ki_ang = 0.001;
+    Ki_fwd = 0.0001;
+
+    // Callback updates set point position in x and y
+    sub_position = nh.subscribe("pathplanner/x_y_yaw",1,&Control::get_position,this);
+    sub_setpoint = nh.subscribe("/pathplanner/setpoint_smooth",1,&Control::get_setpoint, this);
+
+    pub_control_speed = nh.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity",1);
+}
+
+//Subscribes to current position
+void Control::get_position(const geometry_msgs::Point& posMsg) {
+
+    pos_x = posMsg.x;
+    pos_y = posMsg.y;
+    heading = posMsg.z;
+
+}
+
+//This function will subscribe to setpoint messages from
+//the pathplanner node.
+void Control::get_setpoint(const geometry_msgs::Point& setpointMsg) {
+
+    //Position from pathplanner node
+    setpoint_x = setpointMsg.x;
+    setpoint_y = setpointMsg.y;
+    setpoint_heading = atan2(error_x, error_y);
+
+    //Calculate setpoint heading
+    error_x = (setpoint_x - pos_x);
+    error_y = (setpoint_y - pos_y);
+    error_pos = sqrt(error_x * error_x + error_y * error_y);
+    error_heading = setpoint_heading - heading;
+
+    // Yaw angle is defined as [0, 2pi], so the residuals can not be less than or grater than +-pi:
+    if (error_heading < -PI)
+    {
+        error_heading += 2*PI;
+    }
+    if (error_heading >= PI)
+    {
+        error_heading -= 2*PI;
+    }
+
+    calculate_speed();
+}
+
+void Control::calculate_speed(){
+    geometry_msgs::Twist base_cmd;
 
 
-            //Constructor
-            Control::Control(ros::NodeHandle nh) {
+    I_heading += dt * error_heading;
+    I_pos += dt * error_pos;
 
-                //Initialize
-                pos_x = 0;
-                pos_y = 0;
-                ori_z = 0;
-                ang_z = 0;
-                set_x = 0;
-                set_y = 0;
-                set_z = 0;
-                error_x = 0;
-                error_y = 0;
-                error_z = 0;
-                error_dist = 0;
+    //PI-controller
+    trans_heading = -Kp_ang * error_heading - Ki_ang * I_heading;
+    trans_x = Kp_fwd * error_pos + Ki_fwd * I_pos;
 
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << "(x,y) = (" << pos_x << ", " << pos_y << ") " << std::endl;
+    std::cout << "Setpoint (x,y) = (" << setpoint_x << ", " << setpoint_y << ") " << std::endl;
+    std::cout << "error_x: " << error_x << std::endl;
+    std::cout << "error_y: " << error_y << std::endl;
+    std::cout << "error_pos " << error_pos << std::endl;
+    std::cout << "heading: " << heading << " [rad] = " << heading * 180.0 / PI << " [deg] " << std::endl;
+    std::cout << "setpoint_heading: " << setpoint_heading << " [rad] = " << setpoint_heading * 180.0 / PI << " [deg] " << std::endl;
+    std::cout << "error_heading " << error_heading << " [rad] = " << error_heading * 180.0 / PI << " [deg] " << std::endl;
+    std::cout << "trans_heading: " << trans_heading * 180.0 / PI << " deg/s " << std::endl;
+    std::cout << "trans_heading: " << trans_x << " m/s " << std::endl;
 
-                PI = 3.14159265359;
-                //callback updates set point position in x and y
-                sub_position = nh.subscribe("pathplanner/x_y_yaw",1,&Control::get_position,this);
-                sub_setpoint = nh.subscribe("/pathplanner/setpoint_smooth",1,&Control::get_setpoint, this);
+    if (fabs(error_heading) > 0.1){
+        if (fabs(trans_heading) > 10 * PI / 180.0){
+            trans_heading = 10 * PI / 180.0 * trans_heading / fabs(trans_heading);
+        }
+        std::cout << "Heading control active - commanded heading rate: " << trans_heading * 180.0 / PI << " deg/s " << std::endl;
+        base_cmd.angular.z = trans_heading;
+        base_cmd.linear.x = 0;
+    }
 
-
-                //pub
-                pub_control_speed = nh.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity",1);
-
-
-            }
-
-            //Subscribes to current position
-            void Control::get_position(const geometry_msgs::Point& posMsg) {
-
-                //raw position in Euler
-                pos_x = posMsg.x;
-                pos_y = posMsg.y;
-                    ang_z = posMsg.z;
-
-
-                //std::cout << "Position x: " << pos_x << std::endl;
-                //std::cout << "Position y: " << pos_y << std::endl;
-                //std::cout << "Orientation z: " << ang_z << std::endl;
-            }
+    else {
+        if (fabs(trans_x) > 0.5){
+            trans_heading = 0.5 * trans_x / fabs(trans_x);
+        }
+        std::cout << "Velocity control active - commanded velocity: " << trans_x << " m/s " << std::endl;
+        base_cmd.linear.x = trans_x;
+        base_cmd.angular.z = 0;
+    }
+    pub_control_speed.publish(base_cmd);
+}
 
 
+void Control::spin(){
+    ros::Rate loop_rate(1.0 / dt);
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+}
 
-            //This function will subscribe to setpoint messages from
-            //the pathplanner node.
-            void Control::get_setpoint(const geometry_msgs::Point& setpointMsg) {
-
-                //Position from pathplanner node
-                set_x = setpointMsg.x;
-                set_y = setpointMsg.y;
-
-
-                //set_z = setpointMsg.z;  // [-pi,pi]calculate setpoint for the angle
-
-                //std::cout << "setpoint_x: " << set_x << std::endl;
-                //std::cout << "setpoint_y: " << set_y << std::endl;
-                //std::cout << "setpoint_z: " << set_z << std::endl;
-
-
-
-                //Calculate setpoint heading
-                error_x = (set_x - pos_x);
-                error_y = (set_y - pos_y);
-
-                /*
-                if (fabs(error_x) < 0.001 && error_y > 0.1) {
-                    set_z = 2 * atan2(error_x * sin(ang_z / 2), cos(ang_z / 2));
-                }
-                else if(fabs(error_x) < 0.001 && error_y < -0.1){
-
-                    set_z = -2 * atan2(error_x * sin(ang_z/2),cos(ang_z/2));
-                }
-                else{
-                    set_z = atan2(error_y, error_x);
-                }
-
-                 */
-               //run and publish
-
-                 calculate_speed();
-
-
-                //set_z = acos(error_x*sin(ang_z))/();
-            }
-
-
-            void Control::calculate_speed() {
-                /*
-                // Yaw angle is defined as [0, 2pi], so the residuals can not be less than or grater than +-pi:
-                if (error_z < -PI)
-                {
-                    error_z += 2*PI;
-                }
-                if (error_z >= PI)
-                {
-                    error_z -= 2*PI;
-                }
-                */
-
-                error_z = acos(error_y*sin(ang_z)+error_x*cos(ang_z))/sqrt(error_x*error_x + error_y*error_y);
-                error_dist = sqrt(error_x*error_x + error_y*error_y);
-                float theta_z = (error_x*sin(ang_z)+error_y*cos(ang_z))/sqrt(error_x*error_x + error_y*error_y);
-
-
-
-                //P-controller
-                trans_z = Kp_ang * error_z;
-                trans_x = Kp_fwd * error_dist;
-
-
-                std::cout << "error_x " << error_x << std::endl;
-                std::cout << "error_y " << error_y << std::endl;
-                //std::cout << "set_z " << set_z << std::endl;
-                std::cout << "ang_z " << ang_z << std::endl;
-                std::cout << "error_z " << error_z << std::endl;
-                std::cout << "error_dist " << error_dist << std::endl;
-                //std::cout << "abs(error_z) " << fabs(error_z) << std::endl;
-
-                geometry_msgs::Twist base_cmd;
-
-                if (fabs(error_z) > 0.02 ){
-                    base_cmd.angular.z = trans_z;
-                    base_cmd.linear.x = 0;
-                    std::cout << "trans_z: " << trans_z << std::endl;
-                }
-                else {
-                    base_cmd.linear.x = trans_x;
-                    std::cout << "TRANS_X: " << trans_x << std::endl;
-                    trans_x = 0;
-                }
-
-
-                pub_control_speed.publish(base_cmd);
-            }
-
+Control *control;
 
 int main(int argc, char** argv){
-    ros::init(argc, argv, "navigator");
+    ros::init(argc, argv, "node_controller");
     ros::NodeHandle nh;
-    Control control(nh);
+    Control temp_control(nh);
+    control = &temp_control;
+    control->spin();
 
-    ros::spin();
+    //Control control(nh);
+    //ros::spin();
     return 0;
 }
