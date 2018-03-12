9d871c7eab8e8ecc3f3d0c1f28d3f2e2f99765cb
Sondre Haug <sondr.haug@gmail.com>
2018-03-12 18:59:42 +0800

    Stop publish commands when goal reached. Control check velocity constraints. Chenge limits for integral gain

--- src/node_controller.cpp
+++ src/node_controller.cpp
@@ -1,16 +1,15 @@
 #include "ros/ros.h"
 #include <geometry_msgs/Twist.h> //velocities
 #include <nav_msgs/Odometry.h>
-#include <iostream>
 #include <tf/tfMessage.h> //transforms
 #include <tf/transform_datatypes.h>
-#include <cmath>
 #include <sensor_msgs/LaserScan.h>
-#include <algorithm>
-#include <vector>
-#include <math.h>
+
+
 
 using namespace std_msgs;
+
+enum {GOAL_NOT_REACHED = 4, GOAL_REACHED = 5};
 
 class Control{
 private:
@@ -19,9 +18,10 @@ private:
     ros::Subscriber sub_position;
 
     double trans_x, trans_heading, I_heading, I_pos, dt;
-    double pos_x, pos_y, heading, setpoint_x, setpoint_y,setpoint_x_last,setpoint_y_last, setpoint_heading;
-    double error_x,error_y, error_heading, error_pos,error_pos_prev;
+    double pos_x, pos_y, heading, setpoint_x, setpoint_y, setpoint_heading;
+    double error_x,error_y, error_heading, error_pos, error_pos_prev;
     double PI, Kp_fwd, Kp_ang, Ki_ang, Ki_fwd, Kd_fwd;
+    int goal_reached;
 
 public:
     Control(ros::NodeHandle nh);
@@ -54,11 +54,12 @@ Control::Control(ros::NodeHandle nh) {
     I_heading = 0;
     I_pos = 0;
     dt = 0.1; //10Hz
-    Kp_ang = 0.25; //Heading gain     //0.45 -
+    Kp_ang = 0.30; //Heading gain     //0.45 -
     Ki_ang = 0.020;                   //0.015 -
     Kp_fwd = 0.40; //Forward gain     //0.4 -
-    Ki_fwd = 0.025;                    //0.015 -
+    Ki_fwd = 0.025 * 100;                    //0.015 -
     Kd_fwd = 0.09;                    //0.05 -
+    goal_reached = GOAL_NOT_REACHED;
 
     // Callback updates set point position in x and y
     sub_position = nh.subscribe("pathplanner/x_y_yaw",1,&Control::get_position,this);
@@ -81,6 +82,7 @@ void Control::get_setpoint(const geometry_msgs::Point& setpointMsg) {
     //Position from pathplanner node
     setpoint_x = setpointMsg.x;
     setpoint_y = setpointMsg.y;
+    goal_reached = setpointMsg.z;
 
     calculate_speed();
 }
@@ -97,12 +99,6 @@ void Control::calculate_speed(){
     error_pos = sqrt(error_x * error_x + error_y * error_y);
     error_heading = setpoint_heading - heading;
 
-    //if new setpoint, delete integral term
-    if (error_pos > 0.4) {
-        I_heading = 0;
-        I_pos = 0;
-        std::cout << "ERROR POS LARGER THAN 0.4 " << std::endl;
-    }
 
     // Heading is defined as [-pi, pi], so the error can not be less than -pi or greater than pi:
     if (error_heading < -PI)
@@ -118,6 +114,12 @@ void Control::calculate_speed(){
     I_heading += dt * error_heading;
     I_pos += dt * error_pos;
 
+    // If error in position is large, the proportional term will do the job
+    if (error_pos > 0.4) {
+        I_heading = 0;
+        I_pos = 0;
+        std::cout << "ERROR POS LARGER THAN 0.4 " << std::endl;
+    }
 
     std::cout << "Proportional_fwd: " << Kp_fwd * error_pos << std::endl;
     std::cout << "Integral_fwd: " << Ki_fwd * I_pos << std::endl;
@@ -125,13 +127,9 @@ void Control::calculate_speed(){
     std::cout << "Proportional_head: " << -Kp_ang * error_heading<< std::endl;
     std::cout << "Integral_head: " << - Ki_ang * I_heading << std::endl;
 
-
-    //std::cout << "P_heading: " << Kp_ang * error_heading << " , I_heading: " << Ki_ang * I_heading << std::endl;
-    //std::cout << "P_fwd: " << Kp_fwd * error_pos << " , I_fwd: " << I_pos + Kd_fwd << " D_fwd: "  << Kd_fwd * (error_pos_prev - error_pos) << std::endl;
-    //Compute gain
     if (fabs(error_heading) > 0.05) {
 
-        if (fabs(error_heading) > 0.14) {
+        if (fabs(error_heading) > 0.12) {
             //heading
             trans_heading = -Kp_ang * error_heading - Ki_ang * I_heading;
             std::cout << "trans_heading before: " << trans_heading;
@@ -146,11 +144,12 @@ void Control::calculate_speed(){
 
             std::cout << "Trans error 1 " << std::endl;
 
-        }else{
-            //heading
+        }
+        else{
+            // Heading
             trans_heading = -Kp_ang * error_heading - Ki_ang * I_heading;
             trans_heading *= pow(3.0, fabs(trans_heading) + 1.0);
-            //velocity
+            // Velocity
             trans_x = Kp_fwd * error_pos + Ki_fwd * I_pos + Kd_fwd * (error_pos_prev - error_pos);
             trans_x *= (1.0 / pow(1.5, fabs(trans_heading) + 1.0));
             std::cout << "Trans error 2" << std::endl;
@@ -161,38 +160,49 @@ void Control::calculate_speed(){
         trans_x = Kp_fwd * error_pos + Ki_fwd * I_pos + Kd_fwd * (error_pos_prev - error_pos);
     }
 
-   // std::cout << "trans_heading: " << trans_heading * 180.0 / PI << " deg/s " << std::endl;
-   // std::cout << "trans_x: " << trans_x << " m/s " << std::endl;
-
-    /*
-    std::cout << "---------------------------------" << std::endl;
-    std::cout << "(x,y) = (" << pos_x << ", " << pos_y << ") " << std::endl;
-    std::cout << "Setpoint (x,y) = (" << setpoint_x << ", " << setpoint_y << ") " << std::endl;
-    std::cout << "error_x: " << error_x << std::endl;
-    std::cout << "error_y: " << error_y << std::endl;
-    std::cout << "error_pos " << error_pos << std::endl;
-    std::cout << "heading: " << heading << " [rad] = " << heading * 180.0 / PI << " [deg] " << std::endl;
-    std::cout << "setpoint_heading: " << setpoint_heading << " [rad] = " << setpoint_heading * 180.0 / PI << " [deg] " << std::endl;
-    std::cout << "error_heading " << error_heading << " [rad] = " << error_heading * 180.0 / PI << " [deg] " << std::endl;
-    std::cout << "trans_heading: " << trans_heading * 180.0 / PI << " deg/s " << std::endl;
-    std::cout << "trans_x: " << trans_x << " m/s " << std::endl;
-*/
-
-    if (floor(pos_x) == 4 && floor(pos_y) == 3){
+    // Goal not reached = 4,
+    if (goal_reached > (GOAL_NOT_REACHED + 0.5)){ // +0.5 to avoid numerical error
         base_cmd.linear.x = 0;
-        base_cmd.angular.z = 0.7;
-    }else {
+        base_cmd.angular.z = 0;
+    }
+    else{
         base_cmd.linear.x = trans_x;
         base_cmd.angular.z = trans_heading;
     }
-        pub_control_speed.publish(base_cmd);
+    std::cout << "goal_reached: " << goal_reached << std::endl;
+    std::cout << "Commanded linear velocity: " << trans_x << " m/s " << std::endl;
+    std::cout << "Commanded angular velocity: " << trans_heading * 180.0 / PI << " deg/s " << std::endl;
+
+
+    // Check if commanded velocity exceeds limits:
+    double max_trans_vel = 0.7; // Max translational velocity [m/s]
+    double max_rot_vel = 180.0 * (3.14159265 / 180.0); // Max rotational velocity [rad/s]
+    if (base_cmd.linear.x > max_trans_vel){
+        base_cmd.linear.x = max_trans_vel;
+        std::cout << "Linear x velocity exceeds limit" << std::endl;
+    }
+    if (base_cmd.linear.x < -max_trans_vel){
+        base_cmd.linear.x = -max_trans_vel;
+        std::cout << "Linear x velocity exceeds limit" << std::endl;
+    }
+    if (base_cmd.angular.z > max_rot_vel){
+        base_cmd.angular.z = max_rot_vel;
+        std::cout << "Angular z velocity exceeds limit" << std::endl;
+    }
+    if (base_cmd.angular.z < -max_rot_vel){
+        base_cmd.angular.z = -max_rot_vel;
+        std::cout << "Angular z velocity exceeds limit" << std::endl;
+    }
+
+
+    pub_control_speed.publish(base_cmd);
 }
+
 
 
 void Control::spin(){
     ros::Rate loop_rate(1.0 / dt);
-    while (ros::ok())
-    {
+    while (ros::ok()) {
         ros::spinOnce();
         loop_rate.sleep();
     }
